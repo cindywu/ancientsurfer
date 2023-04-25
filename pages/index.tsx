93cef171172e9ba790609025287ff1cf6c886f77
cindy <1177031+cindywu@users.noreply.github.com>
2023-04-24 20:28:20 -1000

    lettuce begin

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className={"p-4"}>
+      <div>
+        first is the worst
+      </div>
+      <div>
+        second is the best
+      </div>
+      <div>
+        third is the one with the hairy hairy chest
+      </div>
+      <div className={"pt-4"}>are you one of the ancients?</div>
+    </div>
+  )
+}
